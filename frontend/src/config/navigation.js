b7cd00fc9d49b45ee80a92acba304dfb2fb0a6e4
mcnsgit <cardigamiguel221@gmail.com>
2024-11-27 20:08:28 +0000

    render.yalm

--- frontend/src/config/navigation.js
+++ frontend/src/config/navigation.js
@@ -1,6 +1,7 @@
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined';
-// import AccessTimeIcon from '@mui/icons-material/AccessTime';
+// import AccessTimeIcon from '@mui/iconcd frontend
+
 
 export const NAVIGATION = [
   {
